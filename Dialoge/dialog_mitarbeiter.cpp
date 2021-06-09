e605d9dbe8ae99656ed69c559ed55698aa8d977b
TischlerWilly <oliver.schuft@gmx.de>
2021-06-09 16:59:40 +0200

    dlg mitarbeiter

--- Dialoge/dialog_mitarbeiter.cpp
+++ Dialoge/dialog_mitarbeiter.cpp
@@ -6,9 +6,83 @@ Dialog_Mitarbeiter::Dialog_Mitarbeiter(QWidget *parent) :
     ui(new Ui::Dialog_Mitarbeiter)
 {
     ui->setupUi(this);
+    ui->tableWidget_mitarb->setContextMenuPolicy(Qt::CustomContextMenu);
+    this->setWindowTitle("Kostenstellen verwalten");
+    this->setModal(true);
+    //this->setFixedWidth(500);
 }
-
 Dialog_Mitarbeiter::~Dialog_Mitarbeiter()
 {
     delete ui;
 }
+void Dialog_Mitarbeiter::showEvent(QShowEvent *e)
+{
+    Mitarb.initialisieren();
+    update_tablewidget();
+    QDialog::showEvent(e);
+}
+void Dialog_Mitarbeiter::resizeEvent(QResizeEvent *event)
+{
+
+
+    QDialog::resizeEvent(event);
+}
+void Dialog_Mitarbeiter::mitarb_sortieren()
+{
+    Mitarb.sortieren();
+    update_tablewidget();
+}
+void Dialog_Mitarbeiter::on_pushButton_ok_clicked()
+{
+    Mitarb.speichern();
+    this->hide();
+    emit(signal_mitarb(Mitarb));
+}
+void Dialog_Mitarbeiter::on_pushButton_abbrechen_clicked()
+{
+    this->close();
+}
+void Dialog_Mitarbeiter::on_pushButton_neu_clicked()
+{
+
+}
+void Dialog_Mitarbeiter::on_pushButton_edit_clicked()
+{
+
+}
+void Dialog_Mitarbeiter::on_pushButton_entfernen_clicked()
+{
+
+}
+void Dialog_Mitarbeiter::on_tableWidget_kost_customContextMenuRequested(const QPoint &pos)
+{
+    QMenu m(this);
+    QPoint mpos;
+    mpos.setX(pos.x()+this->x());
+    mpos.setY(pos.y()+this->y());
+    m.addAction("Mitarbeiterliste sortieren", this, SLOT(mitarb_sortieren()), 0) ;
+    m.exec(this->mapFrom(this, mpos));
+}
+void Dialog_Mitarbeiter::update_tablewidget()
+{
+    ui->tableWidget_mitarb->clear();
+    ui->tableWidget_mitarb->setColumnCount(Mitarb.tabelle()->anz_spalten());//Spaltenanzahl
+    ui->tableWidget_mitarb->setRowCount(Mitarb.tabelle()->anz_zeilen());//Zeilenanzahl
+    ui->tableWidget_mitarb->setHorizontalHeaderLabels(Mitarb.tabelle()->tabkopf().qstringlist());
+    ui->tableWidget_mitarb->setColumnWidth(0,60);//Pers.Nr.
+    ui->tableWidget_mitarb->setColumnWidth(1,300);//Vorname
+    ui->tableWidget_mitarb->setColumnWidth(2,300);//Nachname
+    ui->tableWidget_mitarb->setColumnWidth(3,100);//ID-Scanner
+
+    for(int i=0;i<Mitarb.tabelle()->anz_zeilen();i++)
+    {
+        for(int ii=0;ii<=Mitarb.tabelle()->anz_spalten();ii++)
+        {
+            ui->tableWidget_mitarb->setItem(i,ii, new QTableWidgetItem(Mitarb.tabelle()->wert(i,ii)));
+        }
+    }
+}
+
+
+
+
--- Dialoge/dialog_mitarbeiter.h
+++ Dialoge/dialog_mitarbeiter.h
@@ -2,6 +2,10 @@
 #define DIALOG_MITARBEITER_H
 
 #include <QDialog>
+#include <QMenu>
+
+#include "Klassen/mitarbeiter.h"
+
 
 namespace Ui {
 class Dialog_Mitarbeiter;
@@ -16,7 +20,28 @@ public:
     ~Dialog_Mitarbeiter();
 
 private:
+    void update_tablewidget();
+    void mitarb_sortieren();
+
+private slots:
+    void resizeEvent(QResizeEvent *event);
+    void on_pushButton_ok_clicked();
+    void on_pushButton_abbrechen_clicked();
+    void on_pushButton_neu_clicked();
+    void on_pushButton_edit_clicked();
+    void on_pushButton_entfernen_clicked();
+    void on_tableWidget_kost_customContextMenuRequested(const QPoint &pos);
+
+signals:
+    void signal_mitarb(mitarbeiter);
+
+private:
     Ui::Dialog_Mitarbeiter *ui;
+    mitarbeiter Mitarb;
+
+protected:
+ void showEvent(QShowEvent *e);
+
 };
 
 #endif // DIALOG_MITARBEITER_H
